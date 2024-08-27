5461d783310902695c5b6c410ecbc222d861ca56
Pauline <eniluap.truocaled@gmail.com>
2024-08-27 13:30:51 +0200

    feat: Etape 2

--- app/controllers/quizController.js
+++ app/controllers/quizController.js
@@ -1,4 +1,4 @@
-const { User, Quiz, Tag } = require('../models');
+const { User, Quiz, Tag, Question, Level } = require('../models');
 
 const quizController = {
     async index(req, res) {
@@ -7,6 +7,22 @@ const quizController = {
         });
         res.render('index', { quizzes: quizzes });
     },
+    async findOne(req, res) {
+        const { id } = req.params;
+
+        const quiz = await Quiz.findOne({
+           where: { id: id },
+           include: ['author', 'tags', 'questions'],
+       });
+
+       const questions = await Question.findAll({
+        where: { quiz_id: id },
+        include: ['level', 'answers'],
+       });
+
+       console.log(questions[0].level);
+       res.render('quiz', { quiz: quiz, questions: questions });
+    }
 
 };
 
--- app/routers/quizRouter.js
+++ app/routers/quizRouter.js
@@ -2,5 +2,6 @@ const quizRouter = require('express').Router();
 const { quizController } = require('../controllers/quizController.js');
 
 quizRouter.get('/', quizController.index);
+quizRouter.get('/quiz/:id(\\d+)', quizController.findOne);
 
 module.exports = { quizRouter };
